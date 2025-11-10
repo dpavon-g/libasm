a722ddc3e76bcbe9f656ae639e1cceb414247386
Daniel Pavon gomez <dpavon-g@c3r9s3.42malaga.com>
2025-11-10 14:19:27 +0100

    Codigo en proceso

--- src/strlen.asm
+++ src/strlen.asm
@@ -2,11 +2,6 @@ section .text
 global ft_strlen
 
 ft_strlen:
-    xor rax, rax           ; Set RAX (length) to 0
-.next_char:
-    cmp byte [rdi + rax], 0 ; Compare current byte with 0
-    je .done               ; If 0, jump to done
-    inc rax                ; Increment RAX
-    jmp .next_char         ; Repeat
-.done:
-    ret                    ; Return length in RAX
+    mov rax, 0;
+    .next_char:
+        cmp byte [rdi + rax], 0
