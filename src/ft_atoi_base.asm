bc07b79a080c3f9ff48c2d18154f47d8374fc8bd
dpavon-g <pavongomezdaniel@gmail.com>
2026-06-17 22:53:02 +0200

    Se comprueban chars inválidos en base

--- src/ft_atoi_base.asm
+++ src/ft_atoi_base.asm
@@ -1,3 +1,7 @@
+section .text
+    global ft_atoi_base
+    extern ft_strlen
+
 ft_atoi_base:
 
     .comprobar_longitud_base:
@@ -6,13 +10,59 @@ ft_atoi_base:
         call ft_strlen
         cmp rax, 1
         jbe .retornar_cero
+        jmp .buscar_invalid_chars
 
     .retornar_cero:
         xor rax, rax
+        jmp .fin
+
+    .retornar_uno:
+        mov rax, 1
+        jmp .fin
+
+    .buscar_invalid_chars:
+        mov rdi, rsi
+        mov rax, 0
+
+        .next_char:
+            cmp byte [rdi + rax], 0
+            je .retornar_uno
+
+            mov cl, byte [rdi + rax]
+            cmp cl, 43
+                je .retornar_cero
+            cmp cl, 45
+                je .retornar_cero
+            cmp cl, 9
+                jl .comprobar_duplicado
+            cmp cl, 13
+                jle .retornar_cero
+            
+            jmp .comprobar_duplicado
+
+            .siguiente_char:
+                inc rax
+                jmp .next_char
+
+    .comprobar_duplicado:
+        push rax
+        .incrementar_busqueda_duplicado:
+            inc rax
+            cmp byte [rdi + rax], 0
+                je .fin_comprobar_duplicado
+
+            cmp cl, byte [rdi + rax]
+                jne .incrementar_busqueda_duplicado
+
+            pop rax
+            jmp .retornar_cero
+
+            .fin_comprobar_duplicado:
+                pop rax
+                jmp .siguiente_char
+
     .fin:
         pop rdi
-        mov rsp, rbp
-        pop rbp
         ret
     
     
